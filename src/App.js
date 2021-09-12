11d2daaa0fd29f3b47b06c4e1ee0a6625ae3e834
L1ndseyHerman <lherman21@jcu.edu>
2021-09-12 19:46:14 -0400

    Caught up to vid175, which involves sending an HTTP request to the SWAPI (an online Star Wars back-end API) to get data about the Star Wars movies!

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+
+  const [movies, setMovies] = useState([]);
+
+  function fetchMoviesHandler() {
+    //  This can take a second object parameter with options, but don't need it here.
+    //  fetch() returns a Promise.
+    //  Is asynchronous automatically?!
+    //  Remember, .then() has something to do w try/catch.
+    fetch('https://swapi.dev/api/films').then(response => {
+      //  response is an object.
+      //  SWAPI data is coming in in JSON form, so easy to turn into JS object!
+      //  .json() is a METHOD?! Converts JSON to an object or something.
+      return response.json();
+    }).then(data => {
+      //  Changing the names of some movie properties from what they are in the database
+      //  to what u want them to be:
+      const transformedMovies = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        };
+      });
+      //  Just want the results array from the data, so here it is:
+      setMovies(transformedMovies);
+    });
+
+    //  Could have an "options" object param, but don't need here, bec the default 
+    //  method is "get", which is right for this.
+    /*fetch('https://swapi.dev/api/films', {
+      method
+    });*/
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
