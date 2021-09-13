95fa15a408e85ec3b5a2a7e3ad7259a04f737295
L1ndseyHerman <lherman21@jcu.edu>
2021-09-12 20:08:29 -0400

    Caught up to vid177 with loading and no data found messages.

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ import './App.css';
 function App() {
 
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   //  Replacing lots of .then() with async and await.
   async function fetchMoviesHandler() {
+
+    setIsLoading(true);
+
     //  Anything with an "await" runs on an asynchronous thread or something.
     const response = await fetch('https://swapi.dev/api/films');
     const data = await response.json();
@@ -24,6 +28,7 @@ function App() {
       });
       //  Just want the results array from the data, so here it is:
       setMovies(transformedMovies);
+      setIsLoading(false);
   }
 
   return (
@@ -32,7 +37,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
